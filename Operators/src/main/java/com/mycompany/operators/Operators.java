7a74ee01dc0b85e5776e4555b2bd71b3884a296f
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-08-29 10:35:54 +0300

    Operators

---
+++ Operators/src/main/java/com/mycompany/operators/Operators.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operators;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Operators {
+
+    public static void main(String[] args) {
+       
+        /*
+        + ---> Toplama Operatörü
+        - ---> Çıkarma Operatörü
+        / ---> Bölme Operatörü
+        * ---> Çarpma Operatörü
+        % ---> Kalan Operatörü
+        */
+        
+        /*
+        System.out.println(3 + 4.2);
+        System.out.println(3 - 5f);
+        System.out.println(10 / 4.0);
+        System.out.println(3 * 4.5);
+        System.out.println(10 % 4);
+        */
+        
+        int a = 4;
+        a += 2;  // a = a + 2 demektir...
+        System.out.println(a);
+        
+        a++; //+1
+        a--; //-1
+             
+    }
+}
