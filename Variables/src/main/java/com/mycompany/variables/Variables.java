2cbc0cf8fab061975ed6d73f097a86ea1a1c82e8
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-08-29 04:35:03 +0300

    variables

---
+++ Variables/src/main/java/com/mycompany/variables/Variables.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variables;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        
+        int a = 4;
+        System.out.println(a);
+    }
+}
