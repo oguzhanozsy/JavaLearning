87ea77848e17297367bb45be92dea708c7e20842
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-08-29 04:50:43 +0300

    variables

--- Variables/src/main/java/com/mycompany/variables/Variables.java
+++ Variables/src/main/java/com/mycompany/variables/Variables.java
@@ -11,8 +11,40 @@ package com.mycompany.variables;
 public class Variables {
 
     public static void main(String[] args) {
-        
+    
+    /*    
         int a = 4;
         System.out.println(a);
+    */
+    
+    /*
+        int a = 4;
+        a = 10;
+        System.out.println(a);
+    */
+    
+    /*
+        int a = 5;
+        int b = 3 + 5;
+        System.out.println(a);
+        System.out.println(b);
+    */
+    
+    /*
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        
+        int toplam = a + 2*b + c;
+        
+        System.out.println("Toplam = " + toplam);
+    */
+        
+        int a = 10;
+        int b = a * 2;
+        int c = b - 3;
+        int toplam = 2*a + b - c;
+        
+        System.out.println("Toplam = " + toplam);
     }
 }
