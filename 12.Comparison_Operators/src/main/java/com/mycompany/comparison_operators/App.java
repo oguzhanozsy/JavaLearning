9883763c2f06a0dd186f9c6faf8f12126c1af7ca
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-09-03 12:21:20 +0300

    comparison operators

---
+++ 12.Comparison_Operators/src/main/java/com/mycompany/comparison_operators/App.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.comparison_operators;
+
+/**
+ *
+ * @author ASUS
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        /*
+            KARŞILAŞTIRMA OPERATÖRLERİ
+        
+            == ---> Eşit mi ?
+            != ---> Eşit değil mi ?
+            > ---> Büyük mü ?
+            >= ---> Büyük veya eşit mi ?
+            < ---> Küçük mü ?
+            <= ---> Küçük veya eşit mi ?
+        */
+        
+        System.out.println(3 == 4);
+        System.out.println(3 != 4);
+        System.out.println(2 > 1);
+        System.out.println(2 >= 3);
+        System.out.println(2 < 1);
+        System.out.println(2 <= 1);
+        System.out.println("Oğuzhan" == "Oğuzhan");
+    }
+}
