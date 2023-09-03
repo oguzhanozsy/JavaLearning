8c3adeee1200ae4c0ff389cf60dcb71b021a45ec
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-09-03 11:47:57 +0300

    Strings

---
+++ 5.Strings/src/main/java/com/mycompany/strings/Strings.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.strings;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Strings {
+
+    public static void main(String[] args) {
+        /*
+        String a = "Merhaba";
+        System.out.println(a + " Hoşgeldiniz...");
+        */
+        
+        /*
+        String x = "Java ";
+        String y = "Programlama ";
+        String z = "Dili ";
+        String toplam = x + y + z;
+        
+        System.out.println("Toplam " + toplam);
+        */
+        
+        /*
+        String a = "Merhaba ";
+        int b = 35;
+        String c = a + b;
+        System.out.println(c);
+        
+        double x = 3.52;
+        byte y = 10;
+        a = a + x + y;
+        System.out.println(a);
+        */
+    
+        /*
+        char a = '?';
+        String b = "Merhaba Nasılsın" + a;
+        System.out.println(b);
+        */
+        
+        String a = "Java " +
+                   "Programlama " +
+                    "Dili";
+        System.out.println(a);
+        
+        String x = "Java\nProgramlama\tDili";
+        System.out.println(x);
+        
+    }
+}
