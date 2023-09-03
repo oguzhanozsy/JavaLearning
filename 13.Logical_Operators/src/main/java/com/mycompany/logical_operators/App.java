1db64178e20b118c27e7a973c4ce789b38636039
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-09-03 13:43:56 +0300

    logical operators

---
+++ 13.Logical_Operators/src/main/java/com/mycompany/logical_operators/App.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.logical_operators;
+
+/**
+ *
+ * @author ASUS
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        /*
+        Mantıksal Operatörler
+        && --> and Operatörü
+        Bütün sonuçlar kendi içinde true ise genel sonuç true, en az birisi bile false ise genel sonuç false olur.
+        
+        || --> or Operatörü
+        Sonuçlardan en az birisi bile true ise genel sonuç true, hepsi false ise genel sonuç false olur.
+        
+        ! --> not Operatörü
+        */
+        
+        System.out.println(3 == 3);
+        System.out.println(2 < 3);
+        System.out.println("Murat" == "Murat");
+        
+        System.out.println(3 != 3 || 2 > 3 || "Murat" == "Murat");
+        System.out.println(!(3 < 4));
+        
+        System.out.println(!((3 < 4 && "Murat" == "Murat") || 3.4 > 2.1));
+    }
+}
