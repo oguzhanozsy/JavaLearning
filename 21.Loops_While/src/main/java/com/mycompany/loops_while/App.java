d4d1a9364922f98b9946224f457d800f8fb54312
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-09-06 12:29:30 +0300

    loops(do)

---
+++ 21.Loops_While/src/main/java/com/mycompany/loops_while/App.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loops_while;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        /*
+        int i = 10;
+        
+        while(i < 100){
+            
+            System.out.println("i = " + i);
+            i += 3;
+            
+        }
+        */
+        
+        /*
+        Scanner a = new Scanner(System.in);
+        
+        System.out.print("Bir Sayi Gİriniz:");
+        
+        int sayi = a.nextInt();
+        int faktoriyel = 1;
+        
+        while (sayi > 0){
+            
+            faktoriyel *= sayi;
+            sayi--;
+            
+        }
+        
+        System.out.println("Faktoriyel = " + faktoriyel);
+        */
+        
+        
+        //SONSUZ DÖNGÜ...
+        int i = 0;
+        
+        while(i < 10){
+            
+            System.out.println("i = " + i);
+            
+        }
+        
+    }
+}
