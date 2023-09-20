a2c4fd20999891c250050942b139a7a04eee7623
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-09-20 18:33:09 +0300

    The Multiplication Table

---
+++ 26.Mini_Project3/src/main/java/com/mycompany/mini_project3/App.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mini_project3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        for(int i = 1 ; i <= 10; i++){
+            
+            System.out.println("********************");
+            
+            for(int j = 1; j <= 10; j++){
+                
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println("********************");
+        }
+        
+    }
+}
