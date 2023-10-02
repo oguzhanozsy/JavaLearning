e6633e7a9108adca81ae785a557e36018974710b
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-10-02 16:39:36 +0300

    dasdas

---
+++ dasda/src/main/java/com/mycompany/dasda/Dasda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dasda;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Dasda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
