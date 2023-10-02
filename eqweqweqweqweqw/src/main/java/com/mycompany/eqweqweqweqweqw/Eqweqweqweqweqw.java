0cfd760d71d2cdc58d720a9626ed4d330938738b
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-10-02 16:34:48 +0300

    eqweqweqwewq

---
+++ eqweqweqweqweqw/src/main/java/com/mycompany/eqweqweqweqweqw/Eqweqweqweqweqw.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eqweqweqweqweqw;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Eqweqweqweqweqw {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
