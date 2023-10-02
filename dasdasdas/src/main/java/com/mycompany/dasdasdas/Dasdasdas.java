3c6f0080ded63fdc9c66c8e6644f1a6ea04ed825
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-10-02 16:39:09 +0300

    eqweqweqwewq

---
+++ dasdasdas/src/main/java/com/mycompany/dasdasdas/Dasdasdas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dasdasdas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Dasdasdas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
