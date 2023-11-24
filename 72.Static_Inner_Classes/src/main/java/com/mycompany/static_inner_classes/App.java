7772eca9b33aa1faad86125cd35df009c5336266
oguzhanozsy <oguzhanozsy37@gmail.com>
2023-11-24 11:34:32 +0200

    static inner class

---
+++ 72.Static_Inner_Classes/src/main/java/com/mycompany/static_inner_classes/App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.static_inner_classes;
+
+/**
+ *
+ * @author ASUS
+ */
+public class App {
+
+    public static void main(String[] args) {
+       
+        Matematik.Alan alan = new Matematik.Alan();
+        
+        alan.daire_alan(5);
+        Matematik.Alan.daire_alan(6);
+    }
+}
---
+++ 72.Static_Inner_Classes/src/main/java/com/mycompany/static_inner_classes/Matematik.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.static_inner_classes;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Matematik {
+    
+    private static double PI = Math.PI;
+    
+    public static class Alan{
+        
+        public static void daire_alan(int yaricap){
+            
+            System.out.println("Dairenin Alani: " + (yaricap * yaricap * PI));
+        }
+    }
+    
+}
